5220939b3197524619d6d2ecddd3a195f1bf7937
Maciej Mazurek <mazurekk67@gmail.com>
2025-04-24 12:43:36 +0200

    Add project files.

---
+++ reversi-3-player/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace reversi_3_player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
