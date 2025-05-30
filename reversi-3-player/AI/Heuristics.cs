60ee2de9c93cf0a42375c2c93128a8a855a56111
Janek <Janek@JANEK>
2025-05-30 11:53:46 +0200

    Another Changes to stability

--- reversi-3-player/AI/Heuristics.cs
+++ reversi-3-player/AI/Heuristics.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using reversi_3_player.Domain;
+using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace reversi_3_player.AI
@@ -144,32 +146,104 @@ namespace reversi_3_player.AI
         /// <returns></returns>
         public static int CountStability(GameState state, int player, (int, int) position)
         {
-            int[] DirectionStability = new int[8];
-            int n = 0;
-            for (int i = -1; i < 2; i++)
+            int[] DirectionStability = new int[4];
+
+            int player2 = (player + 1) % 3;
+            int player3 = (player + 2) % 3;
+            player2 = player2 == 0 ? 3 : player2;
+            player3 = player3 == 0 ? 3 : player3;
+
+            bool Zero = false;
+
+            for (int k = 0;k < 4;k++)
             {
-                for (int j = -1; j < 2; j++)
+                (int i, int j) = GetDirection(k);
+                (int x, int y) = position;
+
+                var Out = new bool[2];
+                var Empty = new bool[2];
+                var P2 = new int[2];
+                var P3 = new int[2];
+
+                (Out[0], Empty[0], P2[0], P3[0]) = CheckStabilityDirection(state, player, player2, player3, position, (i, j));
+
+                (Out[1], Empty[1], P2[1], P3[1]) = CheckStabilityDirection(state, player, player2, player3, position, (-i, -j));
+
+                if ((Out[0] && Out[1]) || (Out[0] && P2[0] == 0 && P3[0] == 0) || (Out[1] && P2[1] == 0 && P3[1] == 0))
+                    DirectionStability[k] = 1;
+                else if (((P2[0] == 0 && P2[1] == 0) || (P2[0] > 0 && P2[1] > 0)) &&
+                        ((P3[0] == 0 && P3[1] == 0) || (P3[0] > 0 && P3[1] > 0)))
                 {
-                    if (i == 0 && j == 0)
-                        continue; // pomijamy kierunki ukośne
-                    DirectionStability[n] = CheckStabilityDirection(state, player, position, (i, j));
-                    if (n > 3)
-                        if ((DirectionStability[n] == -1 && DirectionStability[7 - n] == 0) || DirectionStability[n] == 0 && DirectionStability[7 - n] == -1)
-                        {
-                            //Console.WriteLine(-1);
-                            return -1;
-                        }
-                    n++;
+                    if (Out[0] || Out[1])
+                        DirectionStability[k] = 1;
+                    else
+                        DirectionStability[k] = 0;
                 }
-            }
-            for (int i = 0; i < 4; i++)
-                if (DirectionStability[i] == 0 && DirectionStability[7 - i] == 0)
+                else
                 {
-                    //Console.WriteLine(0);
-                    return 0;
+                    DirectionStability[k] = -1;
+                    //for (int l = 0; l < 2; l++)
+                    //{
+                    //    int nextl = (l + 1) % 2;
+                    //    if ((P2[l] > 0 && P2[nextl] == 0 && Empty[nextl]) || (P3[l] > 0 && P3[nextl] == 0 && Empty[nextl]))
+                    //        DirectionStability[k] = -1;
+                    //}
                 }
 
+                if (DirectionStability[k] == -1)
+                    return -1;
+                if (DirectionStability[k] == 0)
+                    Zero = true;
+            }
+
+            if (Zero)
+                return 0;
+
             return 1;
+
+            //int[] DirectionStability = new int[8];
+            //int n = 0;
+            //for (int i = -1; i < 2; i++)
+            //{
+            //    for (int j = -1; j < 2; j++)
+            //    {
+            //        if (i == 0 && j == 0)
+            //            continue; // pomijamy kierunki ukośne
+            //        DirectionStability[n] = CheckStabilityDirection(state, player, position, (i, j));
+            //        if (n > 3)
+            //            if ((DirectionStability[n] == -1 && DirectionStability[7 - n] == 0) || DirectionStability[n] == 0 && DirectionStability[7 - n] == -1)
+            //            {
+            //                //Console.WriteLine(-1);
+            //                return -1;
+            //            }
+            //        n++;
+            //    }
+            //}
+            //for (int i = 0; i < 4; i++)
+            //    if (DirectionStability[i] == 0 && DirectionStability[7 - i] == 0)
+            //    {
+            //        //Console.WriteLine(0);
+            //        return 0;
+            //    }
+
+            //return 1;
+        }
+
+        public static (int x, int y) GetDirection(int x)
+        {
+            switch(x)
+            {
+                case 0:
+                    return (0, 1); // w prawo
+                case 1:
+                    return (1, 1); // w prawo i w górę
+                case 2:
+                    return (1, 0); // w górę
+                case 3:
+                    return (1, -1); // w lewo i w górę
+                default:
+                    return (0, 0);
+            }
         }
 
         /// <summary>
@@ -180,21 +254,28 @@ namespace reversi_3_player.AI
         /// <param name="position"></param>
         /// <param name="direction"></param>
         /// <returns></returns>
-        public static int CheckStabilityDirection(GameState state, int player, (int, int) position, (int, int) direction)
+        public static (bool Out, bool Empty, int P2, int P3) CheckStabilityDirection(GameState state, int player, int player2, int player3, (int, int) position, (int, int) direction)
         {
             (int x, int y) = position;
             (int i, int j) = direction;
 
-            while (CheckIfInSideBoard((x, y)) && state.Board[x, y] == player)
+            int p2 = 0;
+            int p3 = 0;
+
+            while (CheckIfInSideBoard((x, y)) && state.Board[x, y] != 0)
             {
+                if (state.Board[x, y] == player2)
+                    p2++;
+                else if (state.Board[x, y] == player3)
+                    p3++;
                 x += i;
                 y += j;
             }
+
             if (!CheckIfInSideBoard((x, y)))
-                return 1;
-            if (state.Board[x, y] == 0)
-                return 0;
-            return -1;
+                return (true, false, p2, p3);
+
+            return (false, true, p2, p3);
         }
 
         /// <summary>
